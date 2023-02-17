ee1541c99267ecbd269af4905623f042d0018fa5
Elly <122857130+EljonesA@users.noreply.github.com>
2023-02-17 02:29:06 -0800

    if within for

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char al, e, q;
+
+	e = 'e';
+	q = 'q';
+
+	for (al = 'a'; al <= 'z'; al++)
+	{
+		if (al != e && al != q)
+			putchar(al);
+	}
+	putchar('\n');
+
+	return (0);
+}
