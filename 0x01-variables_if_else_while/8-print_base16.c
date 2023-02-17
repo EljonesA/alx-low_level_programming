25d10860048df05c8cf6a988c5007e2fda93deec
Elly <122857130+EljonesA@users.noreply.github.com>
2023-02-17 02:46:51 -0800

    Hexadecimal

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int i;
+	char al;
+
+	for (i = '0'; i <= '9'; i++)
+		putchar(d);
+	for (al = 'a'; al <= 'f'; al++)
+		putchar(al);
+	putchar('\n');
+
+	return (0);
+}
