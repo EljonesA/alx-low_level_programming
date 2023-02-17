93999261176dc2585109f8a6c9e4024ab0e161fb
Elly <122857130+EljonesA@users.noreply.github.com>
2023-02-17 02:20:34 -0800

    alphaBET

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char alP;
+
+	for (alP = 'a'; alP <= 'z'; alP++)
+		putchar(alP);
+	for (alP = 'A'; alP <= 'Z'; alP++)
+		putchar(alP);
+	putchar('\n');
+
+	return (0);
+}
