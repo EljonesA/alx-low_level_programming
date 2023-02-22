040fd0751680fbd9482b0482ab2a66fa794000da
Elly <122857130+EljonesA@users.noreply.github.com>
2023-02-22 15:10:37 +0000

    Print alphabet 10x

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,9 +1,10 @@
 #include "main.h"
 
 /*
- * print_alphabet_x10 - print alphabet in lowercase 10X.
- * 
- * Return: Always 0.
+ * print_alphabet_x10 - check main
+ * Des - prints 10 times the alphabet in small letters
+ *
+ * Return: Always 0 (Success)
  */
 
 void print_alphabet_x10(void)
@@ -14,7 +15,7 @@ void print_alphabet_x10(void)
 	while (a <= 9)
 	{
 		a++;
-	
+
 		for (i = 'a'; i <= 'z'; i++)
 			_putchar(i);
 		_putchar('\n');
