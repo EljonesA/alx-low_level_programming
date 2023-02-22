356a818e31bf35056a1970bccac5f051f462e882
Elly <122857130+EljonesA@users.noreply.github.com>
2023-02-22 16:26:31 +0000

    Compute absolute value

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _abs - in Main
+ * @m - an integer output
+ * Description - the function returns absolute value of a num
+ *
+ * Return: Always 0 (Success)
+ */
+
+int _abs(int m)
+{
+	if (m >= 0)
+		return (m);
+	else
+		return (m * -1);
+}
