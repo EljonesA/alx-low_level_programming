eee45510de7e5505f3bd99670a1ff473686248b5
Elly <122857130+EljonesA@users.noreply.github.com>
2023-02-22 14:50:35 +0000

    Print alphabet 10x

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -2,19 +2,19 @@
 
 /*
  * print_alphabet_x10 - print alphabet in lowercase 10X.
- * Description: followed by a new line
+ * 
  * Return: Always 0.
  */
 
 void print_alphabet_x10(void)
 {
 	int a = 0;
+	char i;
 
 	while (a <= 9)
 	{
 		a++;
-		char i;
-
+	
 		for (i = 'a'; i <= 'z'; i++)
 			_putchar(i);
 		_putchar('\n');
