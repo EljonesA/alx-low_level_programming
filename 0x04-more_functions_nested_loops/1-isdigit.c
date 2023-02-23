ce223e4babebd1ccf1e1fbc92bd1d550751fe103
Elly <122857130+EljonesA@users.noreply.github.com>
2023-02-23 13:05:33 +0000

    Check for digits

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -3,19 +3,23 @@
 /**
  * _isdigit - in Main
  * Description - Checks for a digit (0 through 9)
- * @param c - character to check
+ * @c: character to check
  *
  * Return: 1 or 0 depending on condition
  */
 
 int _isdigit(int c)
 {
-	if (c >= 0 && c <= 9)
+	char i = '0';
+	int isdigit = 0;
+
+	for (; i <= '9'; i++)
 	{
-		return (1);
+		if (i == c)
+		{
+			isdigit = 1;
+			break;
+		}
 	}
-	else
-	{
-		return (0);
-	}
+	return (isdigit);
 }
