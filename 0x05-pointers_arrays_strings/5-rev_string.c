c7d179e72cc595fc907497d0cfe60e3e21bfc771
Elly <122857130+EljonesA@users.noreply.github.com>
2023-02-27 13:00:24 +0000

    Reverse string

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * rev_string - reverses a string
+ * @s: pointer s of type char
+ * Return: void.
+ */
+
+void rev_string(char *s)
+{
+	int len = 0, i = 0;
+	char temp;
+
+	while (s[len] != '\0')
+		len++;
+	while (i < len--)
+	{
+		temp = s[i];
+		s[i++] = s[len];
+		s[len] == temp;
+	}
+}
