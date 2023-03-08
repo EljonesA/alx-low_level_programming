9d1ce4f947e1f9cde10c17af024bdfc7ef35377e
Elly <122857130+EljonesA@users.noreply.github.com>
2023-03-08 10:26:12 +0000

    Get string length

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -9,11 +9,8 @@
 
 int _strlen_recursion(char *s)
 {
-	int len;
-
 	if (*s)
-	{
-		len = 1 + _strlen_recursion(s + 1);
-	}
-	return (len);
+		return (1 + _strlen_recursion(s + 1));
+
+	return (0);
 }
