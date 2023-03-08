ac1f21085af5ab566e5a908a29eff0784c218921
Elly <122857130+EljonesA@users.noreply.github.com>
2023-03-08 09:24:50 +0000

    Print string

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - prints string followed by new line
+ * @s: pointer to a character array
+ *
+ * Return: success (0).
+ */
+
+void _puts_recursion(char *s)
+{
+	if (*s == '\0') /* base case */
+		return;
+
+	_putchar(*s);
+	s++;
+	_puts_recursion(s);
+	_putchar('\0');
+}
