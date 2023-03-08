0be427a6c97815f378a455054bd30e4aacc27917
Elly <122857130+EljonesA@users.noreply.github.com>
2023-03-08 09:35:33 +0000

    Print string

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -9,10 +9,11 @@
 
 void _puts_recursion(char *s)
 {
-	if (*s == '\0') /* base case */
-		return;
-
-	_putchar(*s);
-	_puts_recursion(s + 1);
-	_putchar('\n');
+	if (*s)
+	{
+		_putchar(*s);
+		_puts_recursion(s + 1);
+	}
+	else
+		_putchar('\n');
 }
