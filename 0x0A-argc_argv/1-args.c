a1a41a8c59ff87bab552152d634d465dbabb9417
Elly <122857130+EljonesA@users.noreply.github.com>
2023-03-13 08:01:24 +0000

    Print number of arguements

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - prints number of arguemnets passed to it
+ * @argc: arguement count
+ * @argv: arguemet vector
+ * Return: 0.
+ */
+
+int main(int argc, char *argv[] __attribute__((unused)))
+{
+	printf("%d\n", argc);
+	return (0);
+}
