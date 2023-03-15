c37492d9d2379b1c8c3e0211f843f3395fc646f8
Elly <122857130+EljonesA@users.noreply.github.com>
2023-03-15 17:17:55 +0000

    Points to a 2D array

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,35 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * alloc_grid - returns a pointer to a 2D array of integers
+ * @width: input integer at number of columns
+ * @height: input integer at number of rows
+ *
+ * Return: pointer to a 2D array, NILL on failure
+ */
+
+int **alloc_grid(int width, int height)
+{
+	int **array;
+	int i = 0;
+	int j = 0;
+
+	if (width <= 0 || height <= 0)
+		return (NULL);
+
+	array = (int **)malloc(sizeof(int *) * height);
+	if (array == NULL)
+		return (NULL);
+	for (; i < height; i++)
+	{
+		array[i] = (int *)malloc(sizeof(int) * width);
+		if (array[i] == NULL)
+			return (NULL);
+	}
+
+	for (i = 0; i < height; i++)
+		for (; j < width; j++)
+			array[i][j] = 0;
+	return (array);
+}
