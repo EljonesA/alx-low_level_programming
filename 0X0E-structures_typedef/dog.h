73b59afb467c15978c39b26ee020e2bf6d718968
Elly <122857130+EljonesA@users.noreply.github.com>
2023-03-21 15:22:01 +0000

    Define new struct

---
+++ 0X0E-structures_typedef/dog.h
@@ -0,0 +1,16 @@
+#ifndef _DOG_H_
+#define _DOG_H_
+/**
+ * struct dog - new structure with 3 elements
+ * @name: name of dog
+ * @age: dog's age
+ * @owner: dog's owner
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
