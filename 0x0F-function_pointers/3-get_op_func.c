2ebb9c3ee74a0cdac6daf99e2268400b47e39e90
Elly <122857130+EljonesA@users.noreply.github.com>
2023-03-24 11:46:21 +0000

    Program to perform simple operations

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -8,7 +8,7 @@
  * Return: pointer corresponding to operator given as a parameter.
  */
 
-int (*get_op_func(char *s)(int, int))
+int (*get_op_func(char *s))(int, int)
 {
 	op_t ops[] = {
 		{"+", op_add},
