c5c80554b72b8e0f20dc36fc3089529e8eb42a4d
Elly <122857130+EljonesA@users.noreply.github.com>
2023-03-29 17:14:34 +0000

    Allocate memory

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * malloc_checked - function to allocate memroy using malloc
+ * @b: unsigned input integer
+ * Return: pointer to the allocated memory/ NULL if it fails.
+ */
+
+void *malloc_checked(unsigned int b)
+{
+	void *s;
+
+	s = malloc(b);
+
+	if (s == NULL)
+		exit(98);
+
+	return (s);
+}
---
+++ 0x0C-more_malloc_free/README.md
@@ -0,0 +1,3 @@
+>> The project/tasks help achieve the following objectives <<
+- How to use the exit function
+- What are the functions calloc and realloc from the standard library and how to use them
