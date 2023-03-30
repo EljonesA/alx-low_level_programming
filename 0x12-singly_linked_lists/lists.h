1abaf3a4311d0761a068ff143ebe1bdf221a940b
Elly <122857130+EljonesA@users.noreply.github.com>
2023-03-30 10:48:53 +0000

    Print all elements in a list

---
+++ 0x12-singly_linked_lists/lists.h
@@ -0,0 +1,22 @@
+#ifndef LISTS_H
+#define LISTS_H
+#include <stddef.h>
+
+/**
+ * struct list_s - new list_t structure
+ * @str: input string
+ * @len: input length
+ * @next: pointer to next node.
+ */
+
+typedef struct list_s
+{
+	char *str;
+	unsigned int len;
+	struct list_s *next;
+} list_t;
+
+int _putchar(char c);
+size_t print_list(const list_t *h);
+
+#endif
