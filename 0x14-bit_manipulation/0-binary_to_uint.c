c459079072a898a82578b08127217214f373b052
Elly <122857130+EljonesA@users.noreply.github.com>
2023-04-10 11:18:06 +0000

    Convert binary to unsigned int

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * binary_to_uint - converts binary to unsigned int
+ * @b: pointer to string of 0 & 1 chars
+ *
+ * Return: converted num, 0 incase b is NULL/chars not 0 & 1.
+ */
+
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int outcome = 0;
+	unsigned int base = 0;
+	int i = 0;
+
+	if (b == NULL)
+		return (0);
+	while (b[i])
+		i++;
+	while (i)
+	{
+		if (b[i - 1] != '0' && b[i - 1] != '1')
+			return (0);
+		outcome += (b[i - 1] - '0') * base;
+		base <<= 1;
+		i--;
+	}
+	return (outcome);
+}
---
+++ 0x14-bit_manipulation/README.md
@@ -0,0 +1,4 @@
+>> The project tasks help achieve/understand the following objectives <<
+- Bit manipulation, Bitwise operators
+- Look for the right source of information without too much help
+- How to manipulate bits and use bitwise operators
