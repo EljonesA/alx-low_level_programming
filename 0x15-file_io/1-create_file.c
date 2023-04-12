d4ca9662347b78ea198fe7e772e4532c0979b94d
Elly <122857130+EljonesA@users.noreply.github.com>
2023-04-12 11:39:19 +0000

    Create a file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -28,17 +28,17 @@ int create_file(const char *filename, char *text_content)
 		counter_ptr++;
 	}
 
-	fd = open(filename, O_CREAT | O_TRUNC, 0600);
+	fd = open(filename, O_RDWR | O_CREAT | O_TRUNC, 0600);
 	if (fd < 0)
 		return (-1);
 
 	nwritten = write(fd, text_content, len);
-	if (nwritten < len)
+	if (nwritten < 0)
 	{
 		close(fd);
 		return (-1);
 	}
 
 	close(fd);
-	return (-1);
+	return (1);
 }
