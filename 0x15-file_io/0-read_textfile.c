4e695ed4d0756be3f47dd57b89e9360ede9f0606
Elly <122857130+EljonesA@users.noreply.github.com>
2023-04-12 10:32:52 +0000

    Read text file

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -11,20 +11,28 @@
 ssize_t read_textfile(const char *filename, size_t letters)
 {
 	int fd;
-	char buffer[letters];
+	char *buffer;
 	ssize_t nread;
 	ssize_t nwritten;
+
+	buffer = malloc(sizeof(char) * letters);
+	if (buffer == NULL)
+		return (0);
 
 	if (filename == NULL)
 		return (0);
 
 	fd = open(filename, O_RDONLY);
 	if (fd == -1)
+	{
+		free(buffer);
 		return (0);
+	}
 
 	nread = read(fd, buffer, letters);
 	if (nread == -1)
 	{
+		free(buffer);
 		close(fd);
 		return (0);
 	}
@@ -32,10 +40,12 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	nwritten = write(STDOUT_FILENO, buffer, letters);
 	if ((nwritten == -1) | (nwritten != nread))
 	{
+		free(buffer);
 		close(fd);
 		return (0);
 	}
 
+	free(buffer);
 	close(fd);
 	return (nwritten);
 }
