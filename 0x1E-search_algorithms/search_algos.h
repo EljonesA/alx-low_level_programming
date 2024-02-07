2efb9c093d65d566f3c0c85fbea3b87f545a1761
Elly <122857130+EljonesA@users.noreply.github.com>
2024-02-07 06:16:24 +0300

    Add stddef.h library into header file

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -1,8 +1,10 @@
 #ifndef _SEARCH_ALGOS.H_
 #define _SEARCH_ALGOS.H
 
+#include <stddef.h>
+
 /* Function Prototypes */
 int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
 
-#endif
+#endif /* SEARCH_ALGOS_H */
